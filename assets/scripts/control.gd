43ba47a74c1f05f74a0bca523056a886ae8b9c9d
Zybyte85 <cade.step01@gmail.com>
2024-11-30 19:44:57 -0700

    Added settings, sidebar, and other minor things.

--- assets/scripts/control.gd
+++ assets/scripts/control.gd
@@ -1,8 +1,11 @@
 extends Control
 
-#@onready var editor_scene = preload("res://assets/scenes/editor.tscn")
-@onready var tab_container = $HSplitContainer/TabContainer
-@onready var FileSystem = $HSplitContainer/FileSystem
+#@onready var tab_container = $HSplitContainer/TabContainer
+#@onready var FileSystem = $HSplitContainer/FileSystem
+@onready var tab_container = $HBoxContainer/HSplitContainer/TabContainer
+@onready var FileSystem = $HBoxContainer/HSplitContainer/FileSystem
+
+@onready var config = ConfigFile.new()
 
 func init_settings(editor):
 	#var highlighter = editor.syntax_highlighter
@@ -10,7 +13,7 @@ func init_settings(editor):
 	
 	editor.syntax_highlighter = highlighter
 
-	editor.add_theme_font_size_override("font_size", 22)
+	editor.add_theme_font_size_override("font_size", config.get_value("editor", "font_size"))
 	
 	# Set gutters
 	editor.gutters_draw_breakpoints_gutter = true
@@ -20,16 +23,16 @@ func init_settings(editor):
 	editor.gutters_draw_fold_gutter = true
 	
 	# Small settings
-	editor.code_completion_enabled = true
-	editor.indent_automatic = true
+	editor.code_completion_enabled = config.get_value("editor", "code_completion")
+	editor.indent_automatic = config.get_value("editor", "auto_indent")
 	
-	editor.auto_brace_completion_enabled = true
+	editor.auto_brace_completion_enabled = config.get_value("editor", "brace_completion")
 	editor.auto_brace_completion_highlight_matching = true
-	editor.scroll_smooth = false
-	editor.minimap_draw = true
-	editor.caret_type = 0 # 0 = line 1 = block
-	editor.caret_blink = true
-	editor.draw_tabs = true
+	editor.scroll_smooth = config.get_value("editor", "smooth_scrolling")
+	editor.minimap_draw = config.get_value("editor", "minimap")
+	editor.caret_type = config.get_value("editor", "caret_type") # 0 = line 1 = block
+	editor.caret_blink = config.get_value("editor", "caret_blink")
+	editor.draw_tabs = config.get_value("editor", "tab_icon")
 	
 	# Highlighting
 	highlighter.number_color = Color("#a1ffe0")
@@ -56,13 +59,15 @@ func init_settings(editor):
 	
 	return editor
 
-func add_tab(file):
+func add_tab(file, is_file = true):
 	# Called when a new tab is open
 	var editor = CodeEdit.new()
 	
-	editor.name = file
-	print(editor)
+	editor.name = file.get_file()
 	editor = init_settings(editor)
+	
+	if is_file:
+		editor.text = FileAccess.open(file, FileAccess.READ).get_as_text()
 	
 	print(file + " created")
 	
@@ -73,26 +78,51 @@ func add_tab(file):
 	tab_container.current_tab = tab_container.get_child_count() - 1
 	
 func _ready():
-	initiate_files("res://")
+	initiate_files("user://")
+	
+	if !DirAccess.dir_exists_absolute("user://fonts"):
+		DirAccess.make_dir_absolute("user://fonts")
+
+	var err = config.load("user://settings.ini")
+	if err != OK: 
+		print("Opening config file failed. Generating new one")
+		config.set_value("editor", "font_size", 22)
+		config.set_value("editor", "auto_indent", true)
+		config.set_value("editor", "code_completion", true)
+		config.set_value("editor", "brace_completion", true)
+		config.set_value("editor", "smooth_scrolling", false)
+		config.set_value("editor", "minimap", true)
+		config.set_value("editor", "caret_type", 0)
+		config.set_value("editor", "caret_blink", true)
+		config.set_value("editor", "tab_icon", true)
+		
+		config.save("user://settings.ini")
+	else:
+		print("Opening config file successful")
 	
 func _process(delta):
 	if Input.is_action_just_pressed("New Tab"):
-		add_tab("New Tab")
+		add_tab("New Tab", false)
 	if Input.is_action_just_pressed("Close Tab"):
 		tab_container.get_current_tab_control().queue_free() # Delete selected tab
 	if Input.is_action_just_pressed("Save File"):
-		$FileDialog.show()
+		# Need to fix this later, but I'm to lazy to right now
+		var dialog = FileDialog.new()
+		dialog.file_mode = FileDialog.FILE_MODE_SAVE_FILE
+		dialog.access = FileDialog.ACCESS_FILESYSTEM
+		dialog.use_native_dialog = true
+		dialog.show()
+		print(tab_container.get_current_tab_control().text)
+		#file.store_string(tab_container.get_current_tab_control().text)
 		# Later, fix it so it doesn't still type S (issue #88768 on Godot Github)
-
-func _on_file_dialog_file_selected(path):
-	print(path)
-	var file = FileAccess.open(path, FileAccess.WRITE)
-	file.store_string(tab_container.get_current_tab_control().text)
+	if Input.is_action_just_pressed("Open Settings"):
+		add_tab("user://settings.ini")
 	
-func _on_file_pressed(button):
-	print(button.text)
+#func _on_file_pressed(button):
+#	print(button.text)
 	
 func initiate_files(path):
+	print("Attempting to open " + path)
 	FileSystem.create_item().set_text(0, path)
 	
 	var dir = DirAccess.open(path)
@@ -113,14 +143,14 @@ func initiate_files(path):
 	
 	#item.button_clicked
 
-func open_dir():
-	print("Thingy happened")
-
 func _on_welcome_pressed():
 	var dialog = FileDialog.new()
 	dialog.file_mode = FileDialog.FILE_MODE_OPEN_DIR
 	dialog.access = FileDialog.ACCESS_FILESYSTEM
 	dialog.use_native_dialog = true
-	print(dialog.current_dir)
-	#dialog.dir_selected.connect(open_dir.bind(path))
+	initiate_files(dialog.current_dir)
 	dialog.show()
+
+
+func _on_explorer_button_toggled(toggled_on):
+	$HBoxContainer/HSplitContainer/FileSystem.visible = toggled_on
